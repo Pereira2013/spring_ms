427d9e5bf6a9afac06d34f42894f4101dd2edda6
robsonpereira <robson.pereira@sensedia.com>
2023-10-10 08:52:13 -0300

    feat: wip swagger

--- src/identity/indenty/src/main/java/br/com/Controller.java
+++ src/identity/indenty/src/main/java/br/com/Controller.java
@@ -1,6 +1,12 @@
 package br.com;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("teste")
 public class Controller {
+    @GetMapping
+    @Operation(summary = "GET TESTE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Establishment",
+                    useReturnTypeSchema = true,
+                    content =
+                            {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = BodyTeste.class))}),
+
+            @ApiResponse(responseCode = "400", description = "there was an error in validating the data"),
+            @ApiResponse(responseCode = "500", description = "some unexpected error occurred"),
+    })
     ResponseEntity<?> teste(@RequestBody BodyTeste in) {
         return ResponseEntity.ok("Teste");
     }
---
+++ src/identity/indenty/src/main/java/br/com/infrastructure/http/config/SpringDocConfig.java
@@ -0,0 +1,29 @@
+package br.com.infrastructure.http.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        type = SecuritySchemeType.HTTP,
+        name = "Authorization",
+        scheme = "Bearer")
+public class SpringDocConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Identity Api")
+                        .description("Api responsável para gerenciar usuários ")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+}
