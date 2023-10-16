3eaf486f3073a7dc617278903140ae81d4f76e6c
robsonpereira <robson.pereira@sensedia.com>
2023-10-16 08:15:49 -0300

    feat: POST swagger

--- src/identity/indenty/src/main/java/br/com/Controller.java
+++ src/identity/indenty/src/main/java/br/com/Controller.java
@@ -5,11 +5,9 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("teste")
@@ -27,8 +25,24 @@ public class Controller {
             @ApiResponse(responseCode = "400", description = "there was an error in validating the data"),
             @ApiResponse(responseCode = "500", description = "some unexpected error occurred"),
     })
-    ResponseEntity<?> teste(@RequestBody BodyTeste in) {
+    ResponseEntity<?> getTeste() {
         return ResponseEntity.ok("Teste");
+    }
+    @PostMapping
+    @Operation(summary = "POST TESTE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Created",
+                    useReturnTypeSchema = true,
+                    content =
+                            {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = BodyTeste.class))}),
+
+            @ApiResponse(responseCode = "400", description = "there was an error in validating the data"),
+            @ApiResponse(responseCode = "500", description = "some unexpected error occurred"),
+    })
+    ResponseEntity<?> postTeste(@RequestBody BodyTeste in) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(in);
     }
 
 }
